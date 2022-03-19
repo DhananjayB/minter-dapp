6c103c0038cf2519d613d94303da4eb014f6b42d
DhananjayB <dhananjaybhilare@gmail.com>
2022-03-19 23:25:47 +0530

    updated icons, colors

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -4,7 +4,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 const welcome_h1 = 'Welcome to the Skulled Pumpkins Project!!';
 const welcome_h2 = 'Connect to MetaMask to Get Started';
 const welcome_p =
-  'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+  'This is one of kind project which has collection of unique 10,000 skulled pumpkin mascots. Each mascot is in high quality 3D and randomly generated. So mint your own holiday mascots before its gone!';
 const h1_presale_coming_soon = 'NFT Drop Coming Soon!!';
 const h1_presale_mint = 'Pre-Sale Minting Open!!';
 const h1_public_mint = 'Public Minting Open!!';
